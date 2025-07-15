f47dcbeabc05a16819ac348de8404c4bcefc78b2
Bubalan S <bubalan28@gmail.com>
2025-07-15 22:29:37 +0530

    done

--- Payment_Gateway_Main/src/api.js
+++ Payment_Gateway_Main/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://payment-gateway-backend-juri.onrender.com/api',
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://payment-gateway-backend-juri.onrender.com/api',
+  // baseURL: 'http://localhost:5001/api',
   withCredentials: false // we use Bearer token, not cookies
 });
 
